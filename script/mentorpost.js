4e70f63b9602667efac97f068684156801d42b23
Nguyen Manh Cuong <cuongnmfis@gmail.com>
2014-03-30 20:08:39 +0700

    script

---
+++ script/mentorpost.js
@@ -0,0 +1,68 @@
+db.mentor_post.insert({
+	title: "Marketing Tip",
+	user_id : db.user.find()[1]._id,
+	imagelink:"images/photos/b2.jpg",
+	amazonlink:"http://www.amazon.com/gp/product/0345530918/ref=s9_al_bw_g14_i1?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-3&pf_rd_r=0422FHS25SGTR169YKVJ&pf_rd_t=101&pf_rd_p=1767121342&pf_rd_i=549028",
+	content: "I used this book for one MBA marketing course. It's the Global Edition but I did not find any difference from classmates who had the regular edition (and paid more money). There are no markings in the book and some noticeable wear on the cover. Overall great buy"
+});
+
+db.mentor_post.insert({
+	title: "Sale Management",
+	user_id : db.user.find()[1]._id,
+	imagelink:"images/photos/b3.jpg",
+	amazonlink:"http://www.amazon.com/gp/product/0345530918/ref=s9_al_bw_g14_i1?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-3&pf_rd_r=0422FHS25SGTR169YKVJ&pf_rd_t=101&pf_rd_p=1767121342&pf_rd_i=549028",
+	content: "I used this book for one MBA marketing course. It's the Global Edition but I did not find any difference from classmates who had the regular edition (and paid more money). There are no markings in the book and some noticeable wear on the cover. Overall great buy"
+});
+db.mentor_post.insert({
+	title: "Marketing Strategy",
+	user_id : db.user.find()[0]._id,
+	imagelink:"images/photos/b4.jpg",
+	amazonlink:"http://www.amazon.com/gp/product/0345530918/ref=s9_al_bw_g14_i1?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-3&pf_rd_r=0422FHS25SGTR169YKVJ&pf_rd_t=101&pf_rd_p=1767121342&pf_rd_i=549028",
+	content: "I used this book for one MBA marketing course. It's the Global Edition but I did not find any difference from classmates who had the regular edition (and paid more money). There are no markings in the book and some noticeable wear on the cover. Overall great buy..I used this book for one MBA marketing course. It's the Global Edition but I did not find any difference from classmates who had the regular edition (and paid more money). There are no markings in the book and some noticeable wear on the cover. Overall great buy"
+});
+
+db.mentor_post.insert({
+	title: "Sale Strategy",
+	user_id : db.user.find()[1]._id,
+	imagelink:"images/photos/b5.jpg",
+	amazonlink:"http://www.amazon.com/gp/product/0345530918/ref=s9_al_bw_g14_i1?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-3&pf_rd_r=0422FHS25SGTR169YKVJ&pf_rd_t=101&pf_rd_p=1767121342&pf_rd_i=549028",
+	content: "I used this book for one MBA marketing course. It's the Global Edition but I did not find any difference from classmates who had the regular edition (and paid more money). There are no markings in the book and some noticeable wear on the cover. Overall great buy I used this book for one MBA marketing course. It's the Global Edition but I did not find any difference from classmates who had the regular edition (and paid more money). There are no markings in the book and some noticeable wear on the cover. Overall great buy"
+});
+db.mentor_post.insert({
+	title: "Marketing Management",
+	user_id : db.user.find()[1]._id,
+	imagelink: "images/photos/b6.jpg",
+	amazonlink:"http://www.amazon.com/gp/product/0345530918/ref=s9_al_bw_g14_i1?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-3&pf_rd_r=0422FHS25SGTR169YKVJ&pf_rd_t=101&pf_rd_p=1767121342&pf_rd_i=549028",
+	content: "I used this book for one MBA marketing course. It's the Global Edition but I did not find any difference from classmates who had the regular edition (and paid more money). There are no markings in the book and some noticeable wear on the cover. Overall great buy"
+});
+
+db.mentor_post.insert({
+	title: "Marketing Management",
+	user_id : db.user.find()[1]._id,
+	imagelink: "images/photos/b2.jpg",
+	amazonlink:"http://www.amazon.com/gp/product/0345530918/ref=s9_al_bw_g14_i1?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-3&pf_rd_r=0422FHS25SGTR169YKVJ&pf_rd_t=101&pf_rd_p=1767121342&pf_rd_i=549028",
+	content: "I used this book for one MBA marketing course. It's the Global Edition but I did not find any difference from classmates who had the regular edition (and paid more money). There are no markings in the book and some noticeable wear on the cover. Overall great buy I used this book for one MBA marketing course. It's the Global Edition but I did not find any difference from classmates who had the regular edition (and paid more money). There are no markings in the book and some noticeable wear on the cover. Overall great buyI used this book for one MBA marketing course. It's the Global Edition but I did not find any difference from classmates who had the regular edition (and paid more money). There are no markings in the book and some noticeable wear on the cover. Overall great buy"
+});
+
+db.mentor_post.insert({
+	title: "Marketing Management",
+	user_id : db.user.find()[1]._id,
+	imagelink:"images/photos/b2.jpg",
+	amazonlink:"http://www.amazon.com/gp/product/0345530918/ref=s9_al_bw_g14_i1?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-3&pf_rd_r=0422FHS25SGTR169YKVJ&pf_rd_t=101&pf_rd_p=1767121342&pf_rd_i=549028",
+	content: "I used this book for one MBA marketing course. It's the Global Edition but I did not find any difference from classmates who had the regular edition (and paid more money). There are no markings in the book and some noticeable wear on the cover. Overall great buy I used this book for one MBA marketing course. It's the Global Edition but I did not find any difference from classmates who had the regular edition (and paid more money). There are no markings in the book and some noticeable wear on the cover. Overall great buy"
+});
+db.mentor_post.insert({
+	title: "Marketing Management",
+	user_id : db.user.find()[1]._id,
+	imagelink:"images/photos/b2.jpg",
+	amazonlink:"http://www.amazon.com/gp/product/0345530918/ref=s9_al_bw_g14_i1?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-3&pf_rd_r=0422FHS25SGTR169YKVJ&pf_rd_t=101&pf_rd_p=1767121342&pf_rd_i=549028",
+	content: "I used this book for one MBA marketing course. It's the Global Edition but I did not find any difference from classmates who had the regular edition (and paid more money). There are no markings in the book and some noticeable wear on the cover. Overall great buy"
+});
+
+db.mentor_post.insert({
+	title: "Marketing Management",
+	user_id : db.user.find()[1]._id,
+	imagelink:"images/photos/b2.jpg",
+	amazonlink:"http://www.amazon.com/gp/product/0345530918/ref=s9_al_bw_g14_i1?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-3&pf_rd_r=0422FHS25SGTR169YKVJ&pf_rd_t=101&pf_rd_p=1767121342&pf_rd_i=549028",
+	content: "I used this book for one MBA marketing course. It's the Global Edition but I did not find any difference from classmates who had the regular edition (and paid more money). There are no markings in the book and some noticeable wear on the cover. Overall great buy I used this book for one MBA marketing course. It's the Global Edition but I did not find any difference from classmates who had the regular edition (and paid more money). There are no markings in the book and some noticeable wear on the cover. Overall great buyI used this book for one MBA marketing course. It's the Global Edition but I did not find any difference from classmates who had the regular edition (and paid more money). There are no markings in the book and some noticeable wear on the cover. Overall great buy"
+});
