b4fdf494fb908a4da7dda6157c34785c8d5b6096
cuongnm <cuongnmfis@gmail.com>
2014-04-12 14:56:26 +0700

    Cuongnm

--- myapp/models/MentorPost.py
+++ myapp/models/MentorPost.py
@@ -13,14 +13,15 @@ from mongoengine.fields import StringField, DateTimeField, ReferenceField
 class MentorPost(Document):
 	title = StringField()
 	published_date = DateTimeField(default=datetime.now)
-	from_date = DateTimeField()
-	to_date = DateTimeField()
+	from_date = DateTimeField(default=datetime.now)
+	to_date = DateTimeField(default=datetime.now)
 	place = StringField()
 	user_id = ReferenceField(User)
+	videolink = StringField()
 	imagelink = StringField()
 	amazonlink= StringField()
 	content = StringField()
-	post_type=StringField()
+	post_type=StringField(default=0)
 	status=StringField(default=1)
 	meta = {
 			'ordering': ['-published_date']
