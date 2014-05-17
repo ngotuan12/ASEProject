7db4c90ebc6696811933fe812bd4f92fd2b27d09
cuongnm <cuongnmfis@gmail.com>
2014-05-17 18:06:26 +0700

    df

--- myapp/views/StudentView.py
+++ myapp/views/StudentView.py
@@ -7,13 +7,13 @@ from django.core.context_processors import csrf
 from django.shortcuts import render, render_to_response
 from mongoengine.django.auth import User
 
-from myapp.models.Material import Material
 from myapp.models.CommentPost import CommentPost
 from myapp.models.Curriculumn import Curriculumn
 from myapp.models.Esay import Esay
+from myapp.models.Material import Material
+from myapp.models.Mentor import Mentor
 from myapp.models.MentorPost import MentorPost
 from myapp.util import context_processors
-from myapp.models.Mentor import Mentor
 
 
 @login_required(login_url='/signin')
@@ -22,8 +22,11 @@ def index(request):
 		#mentor_id = '5375ce146c02298c9af00e00'
 		#mentor = Mentor.objects.get(id=mentor_id)
 #		cl = Curriculumn.objects(mentor=mentor)
+		user_id=request.GET['user_id']
+		user = request.user
+		print(user.id)
 		cl = Curriculumn.objects
-		context = {'cl':cl,}
+		context = {'user_id':user_id,'cl':cl,}
 		return render(request,'myapp/studentview.html', context)
 	elif request.method == 'POST':
 		context.update(context_processors.user(request))
