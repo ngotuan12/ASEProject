d62d69276d6cc936f248002a61badfb4497f506c
cuongnm <cuongnmfis@gmail.com>
2014-05-21 15:01:51 +0700

    c

---
+++ script/new/progresstype.js
@@ -0,0 +1,30 @@
+db.progresstype.insert({
+progressName:"Just Start",
+progressDescription:"Just Start",
+rate:"0"
+});
+db.progresstype.insert({
+progressName:"Just Begun",
+progressDescription:"Just Begun",
+rate:"15"
+});
+db.progresstype.insert({
+progressName:"Advanced a bit",
+progressDescription:"Advanced a bit",
+rate:"25"
+});
+db.progresstype.insert({
+progressName:"Half Progress",
+progressDescription:"Half Progress",
+rate:"50"
+});
+db.progresstype.insert({
+progressName:"Almost complete",
+progressDescription:"Almost complete",
+rate:"85"
+});
+db.progresstype.insert({
+progressName:"Complete",
+progressDescription:"Complete",
+rate:"100"
+});
