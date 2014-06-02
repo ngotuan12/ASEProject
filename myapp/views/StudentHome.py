6feac9dd07b50572bcbc62779be15116e3aabc82
cuongnmeas <cuongnm@ex-aritsan.com>
2014-06-02 18:09:07 +0700

    a

--- myapp/views/StudentHome.py
+++ myapp/views/StudentHome.py
@@ -7,16 +7,42 @@ from django.core.context_processors import csrf
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import render, render_to_response
 from mongoengine.django.auth import User
+from mongoengine.queryset.visitor import Q
 
-from myapp.models.CommentPost import CommentPost
-from myapp.models.Esay import Esay
-from myapp.models.MentorPost import MentorPost
+from myapp.models.Curriculumn import Curriculumn
+from myapp.models.CurriculumnLog import CurriculumnLog
+from myapp.models.Material import Material
 from myapp.util import context_processors
 
 
 @login_required(login_url='/signin')
 def index(request):
 	if request.method == 'GET':
-		context = {}
+		#user = request.user
+		user = User.objects.get(username=str(request.user))
+		user_id = user.id
+		print(user_id)
+		print(user)
+		lsCourse = []
+		lsjoined = CurriculumnLog.objects(user_id = user_id)
+		for ls in lsjoined:
+			Course = Curriculumn.objects.get(id=ls.curriculumn.id)
+			lsmt = []
+			for mt in Course.material:
+				mt={"name":mt.name,"description":mt.description}
+				lsmt.append(mt)
+			lsCourseDetail={"name":Course.name,
+							"duration":Course.duration,
+							"duration_type":Course.duration_type,
+							"from_date":Course.from_date,
+							"lsmt":lsmt
+							}
+			lsCourse.append(lsCourseDetail)
+		#List recomment by admin
+		lsRecomment = Curriculumn.objects().all().order_by('published_date')[:5]
+		
+		context = {	'lsCourse':lsCourse,
+					'lsRecomment':lsRecomment
+				}
 		return render(request,'myapp/student-home.html', context)
 	
--- script/new 2/CurriculumnLog.js
+++ script/new 2/CurriculumnLog.js
@@ -2,35 +2,35 @@ db.curriculumnlog.remove();
 db.curriculumnlog.insert({
 published_date : new Date(),
 curriculumn: db.curriculumn.find()[0]._id,
-like : 1,
+process: db.progresstype.find()[0]._id,
 user : db.user.find()[0]._id,
 lastUpdate : new Date(),
 });
 db.curriculumnlog.insert({
 published_date : new Date(),
 curriculumn: db.curriculumn.find()[0]._id,
-like : 1,
+process: db.progresstype.find()[0]._id,
 user : db.user.find()[1],
 lastUpdate : new Date(),
 });
 db.curriculumnlog.insert({
 published_date : new Date(),
 curriculumn: db.curriculumn.find()[0]._id,
-like : 1,
+process: db.progresstype.find()[0]._id,
 user : db.user.find()[2],
 lastUpdate : new Date(),
 });
 db.curriculumnlog.insert({
 published_date : new Date(),
 curriculumn: db.curriculumn.find()[0]._id,
-like : 1,
+process: db.progresstype.find()[0]._id,
 user : db.user.find()[3],
 lastUpdate : new Date(),
 });
 db.curriculumnlog.insert({
 published_date : new Date(),
 curriculumn: db.curriculumn.find()[0]._id,
-like : 1,
+process: db.progresstype.find()[0]._id,
 user : db.user.find()[4],
 lastUpdate : new Date(),
 });
@@ -39,35 +39,35 @@ lastUpdate : new Date(),
 db.curriculumnlog.insert({
 published_date : new Date(),
 curriculumn: db.curriculumn.find()[1],
-like : 1,
+process: db.progresstype.find()[0]._id,
 user : db.user.find()[0]._id,
 lastUpdate : new Date(),
 });
 db.curriculumnlog.insert({
 published_date : new Date(),
 curriculumn: db.curriculumn.find()[1],
-like : 1,
+process: db.progresstype.find()[0]._id,
 user : db.user.find()[1],
 lastUpdate : new Date(),
 });
 db.curriculumnlog.insert({
 published_date : new Date(),
 curriculumn: db.curriculumn.find()[1],
-like : 1,
+process: db.progresstype.find()[0]._id,
 user : db.user.find()[2],
 lastUpdate : new Date(),
 });
 db.curriculumnlog.insert({
 published_date : new Date(),
 curriculumn: db.curriculumn.find()[1],
-like : 1,
+process: db.progresstype.find()[0]._id,
 user : db.user.find()[3],
 lastUpdate : new Date(),
 });
 db.curriculumnlog.insert({
 published_date : new Date(),
 curriculumn: db.curriculumn.find()[1],
-like : 1,
+process: db.progresstype.find()[0]._id,
 user : db.user.find()[4],
 lastUpdate : new Date(),
 });
@@ -76,35 +76,35 @@ lastUpdate : new Date(),
 db.curriculumnlog.insert({
 published_date : new Date(),
 curriculumn: db.curriculumn.find()[2],
-like : 1,
+process: db.progresstype.find()[0]._id,
 user : db.user.find()[0]._id,
 lastUpdate : new Date(),
 });
 db.curriculumnlog.insert({
 published_date : new Date(),
 curriculumn: db.curriculumn.find()[2],
-like : 1,
+process: db.progresstype.find()[0]._id,
 user : db.user.find()[1],
 lastUpdate : new Date(),
 });
 db.curriculumnlog.insert({
 published_date : new Date(),
 curriculumn: db.curriculumn.find()[2],
-like : 1,
+process: db.progresstype.find()[0]._id,
 user : db.user.find()[2],
 lastUpdate : new Date(),
 });
 db.curriculumnlog.insert({
 published_date : new Date(),
 curriculumn: db.curriculumn.find()[2],
-like : 1,
+process: db.progresstype.find()[0]._id,
 user : db.user.find()[3],
 lastUpdate : new Date(),
 });
 db.curriculumnlog.insert({
 published_date : new Date(),
 curriculumn: db.curriculumn.find()[2],
-like : 1,
+process: db.progresstype.find()[0]._id,
 user : db.user.find()[4],
 lastUpdate : new Date(),
 });
@@ -113,35 +113,35 @@ lastUpdate : new Date(),
 db.curriculumnlog.insert({
 published_date : new Date(),
 curriculumn: db.curriculumn.find()[3],
-like : 1,
+process: db.progresstype.find()[0]._id,
 user : db.user.find()[0]._id,
 lastUpdate : new Date(),
 });
 db.curriculumnlog.insert({
 published_date : new Date(),
 curriculumn: db.curriculumn.find()[3],
-like : 1,
+process: db.progresstype.find()[0]._id,
 user : db.user.find()[1],
 lastUpdate : new Date(),
 });
 db.curriculumnlog.insert({
 published_date : new Date(),
 curriculumn: db.curriculumn.find()[3],
-like : 1,
+process: db.progresstype.find()[0]._id,
 user : db.user.find()[2],
 lastUpdate : new Date(),
 });
 db.curriculumnlog.insert({
 published_date : new Date(),
 curriculumn: db.curriculumn.find()[3],
-like : 1,
+process: db.progresstype.find()[0]._id,
 user : db.user.find()[3],
 lastUpdate : new Date(),
 });
 db.curriculumnlog.insert({
 published_date : new Date(),
 curriculumn: db.curriculumn.find()[3],
-like : 1,
+process: db.progresstype.find()[0]._id,
 user : db.user.find()[4],
 lastUpdate : new Date(),
 });
@@ -150,35 +150,35 @@ lastUpdate : new Date(),
 db.curriculumnlog.insert({
 published_date : new Date(),
 curriculumn: db.curriculumn.find()[4],
-like : 1,
+process: db.progresstype.find()[0]._id,
 user : db.user.find()[0]._id,
 lastUpdate : new Date(),
 });
 db.curriculumnlog.insert({
 published_date : new Date(),
 curriculumn: db.curriculumn.find()[4],
-like : 1,
+process: db.progresstype.find()[0]._id,
 user : db.user.find()[1],
 lastUpdate : new Date(),
 });
 db.curriculumnlog.insert({
 published_date : new Date(),
 curriculumn: db.curriculumn.find()[4],
-like : 1,
+process: db.progresstype.find()[0]._id,
 user : db.user.find()[2],
 lastUpdate : new Date(),
 });
 db.curriculumnlog.insert({
 published_date : new Date(),
 curriculumn: db.curriculumn.find()[4],
-like : 1,
+process: db.progresstype.find()[0]._id,
 user : db.user.find()[3],
 lastUpdate : new Date(),
 });
 db.curriculumnlog.insert({
 published_date : new Date(),
 curriculumn: db.curriculumn.find()[4],
-like : 1,
+process: db.progresstype.find()[0]._id,
 user : db.user.find()[4],
 lastUpdate : new Date(),
 });
