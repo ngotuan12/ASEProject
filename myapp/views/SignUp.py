7612a6883f283e49912e70704e3c9e9c03236021
thainn <thainn88@gmail.com>
2014-06-20 14:05:17 +0700

    Fix SignUp.py

--- myapp/views/SignUp.py
+++ myapp/views/SignUp.py
@@ -27,6 +27,11 @@ def signupsns(request):
 		request.session['is_mentor'] = False
 	return HttpResponseRedirect('/student-home')
 def index(request):
+	firstname = "";
+	lastname = "";
+	username = "";
+	password = "";
+	email = "";
 	if request.method == 'GET':
 		return render(request, 'myapp/signup.html', {'rangerDay':range(1,32),'rangerYear':range(2014,1905,-1),})
 	elif request.method == 'POST':
@@ -35,25 +40,41 @@ def index(request):
 		username = request.POST['txtUserName']
 		password = request.POST['txtPassWord']
 		email= request.POST['txtEmail']
-		
-		user = User()
-		user.username = username
-		user.first_name = firstname
-		user.last_name = lastname
-		user.email = email
-		user.set_password(password);
-		user.save()
-		
-		_profile = UserProfile()
-		_profile.user_id = user
-		_profile.save()
-		
-		st = Student()
-		st.user = user
-		st.save()
-		
-		user.backend = 'mongoengine.django.auth.MongoEngineBackend'
-		logout(request)
-		login(request, user)
-		request.session['is_mentor'] = False
-		return HttpResponseRedirect('/home')
+		try:
+			user = User()
+			user.username = username
+			user.first_name = firstname
+			user.last_name = lastname
+			user.email = email
+			user.set_password(password);
+			user.save()
+			
+			_profile = UserProfile()
+			_profile.user_id = user
+			_profile.save()
+			
+			st = Student()
+			st.user = user
+			st.save()
+			
+			user.backend = 'mongoengine.django.auth.MongoEngineBackend'
+			logout(request)
+			login(request, user)
+			request.session['is_mentor'] = False
+			return HttpResponseRedirect('/home')
+		except mongoengine.errors.NotUniqueError as e:
+				return getSignupError(request,'ユーザーはすでに存在しています！',firstname,lastname,username,password,email)
+def getSignupError(request,e,firstname,lastname,username,password,email):
+	c = {
+			'error_message':e,
+			'firstname':firstname,
+			'lastname':lastname,
+			'username':username,
+			'password':password,
+			'email':email,
+			'rangerDay':range(1,31),
+			'rangerYear':range(2014,1905,-1),
+		}
+	c.update(csrf(request))
+	c.update(context_processors.user(request))
+	return render_to_response("myapp/signup.html", c)
