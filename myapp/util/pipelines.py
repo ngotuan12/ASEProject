bf798d8c7d2b2b322881617e714be2d07a9e3c40
thainn <thainn88@gmail.com>
2014-07-15 12:02:05 +0700

    FIx image

--- myapp/util/pipelines.py
+++ myapp/util/pipelines.py
@@ -1,37 +1,31 @@
 from requests import HTTPError
 from myapp.models.UserProfile import UserProfile
-import json
 
 def save_profile_picture(strategy, user, response, details,is_new=False,*args,**kwargs):
 	
 	if strategy.backend.name == 'facebook':
-		url = 'http://graph.facebook.com/{0}/picture'.format(response['id'])
+		imgpro = 'http://graph.facebook.com/{0}/picture'.format(response['id'])
+	elif strategy.backend.name == 'twitter':
+		imgpro = response.get('profile_image_url', '').replace('_normal', '')
+	elif strategy.backend.name == 'google-oauth2' and "picture" in response:
+		imgpro = response['picture']
 	else:
-		url = ""
+		imgpro = ""
 	
-	if strategy.backend.name == 'twitter':
-		url = response.get('profile_image_url', '').replace('_normal', '')
-	else:
-		url = ""
-	
-	if strategy.backend.name == 'google-oauth2' and "picture" in response:
-		url = response['picture']
-	else:
-		url = ""
 	
 	try:
-		if url:
+		if imgpro:
 			try:
 				thisprofile = UserProfile.objects(user_id=user)
 				#ThaiNN Please review this code
 				if len(thisprofile) == 0:
 					upro = UserProfile()
 					upro.user_id = user
-					upro.images = url
+					upro.images = imgpro
 					upro.save()
 				else:
 					mypro = thisprofile[0]
-					mypro.images = url
+					mypro.images = imgpro
 					mypro.save()
 			except Exception as e:
 					print(e)
